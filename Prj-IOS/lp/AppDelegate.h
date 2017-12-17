90d42be1c94f03ab54a4ed1536b18839b79c05cb
jackyu <455501914@qq.com>
2017-12-18 00:04:52 +0800

    add ios implementation and change readme

---
+++ Prj-IOS/lp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  lp
+//
+//  Created by xiaojun on 2017/12/2.
+//  Copyright © 2017年 xiaojun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
