5d749202418de08016d4dd694fe193e935641aa5
tunm <tunmxy@163.com>
2023-03-04 20:49:02 +0800

    Modify model name bug

--- cpp/src/configuration.h
+++ cpp/src/configuration.h
@@ -16,7 +16,7 @@ const std::string DETECT_LOW_HEAD_FILENAME = "b320_header_h.mnn";
 const std::string DETECT_HIGH_BACKBONE_FILENAME = "b640x_backbone_h.mnn";
 const std::string DETECT_HIGH_HEAD_FILENAME = "b640x_head_h.mnn";
 const std::string CLS_MODEL_FILENAME = "litemodel_cls_96xh.mnn";
-const std::string REC_MODEL_FILENAME = "rpv3_mdict_160_r3.mnn";
+const std::string REC_MODEL_FILENAME = "rpv3_mdict_160h.mnn";
 
 const InputSize REC_INPUT_SIZE = {160, 48};
 const InputSize CLS_INPUT_SIZE = {96, 96};
