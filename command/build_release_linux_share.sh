3a2ab20c6b30c334fe20e83c4c389844978579d3
tunmx <tunmxy@163.com>
2023-03-07 14:54:03 +0800

    Linux link Bug

--- command/build_release_linux_share.sh
+++ command/build_release_linux_share.sh
@@ -3,7 +3,7 @@ BUILD_DIR=build/linux
 mkdir -p ${BUILD_DIR}
 cd ${BUILD_DIR}
 
-cmake -DBUILD_SHARE=ON -DBUILD_SAMPLES=OFF -DBUILD_TEST=OFF ../..
+cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARE=ON -DBUILD_SAMPLES=OFF -DBUILD_TEST=OFF ../..
 make -j4
 make install
 
