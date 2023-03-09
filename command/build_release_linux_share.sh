d89904e2bbed5b879ecc6c4dee9dc0d4e6141c17
tunmx <tunmxy@163.com>
2023-03-09 08:48:08 +0800

    Command modify

--- command/build_release_linux_share.sh
+++ command/build_release_linux_share.sh
@@ -1,6 +1,15 @@
 
-BUILD_DIR=build/linux
+uname_s=$(uname -s)
+dir_name="linux"
+if [ "$uname_s" == "Darwin" ];then
+  echo "MacOS"
+  # shellcheck disable=SC2034
+  dir_name="darwin"
+fi
+
+BUILD_DIR=build/${dir_name}
 mkdir -p ${BUILD_DIR}
+# shellcheck disable=SC2164
 cd ${BUILD_DIR}
 
 cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARE=ON -DBUILD_SAMPLES=OFF -DBUILD_TEST=OFF ../..
