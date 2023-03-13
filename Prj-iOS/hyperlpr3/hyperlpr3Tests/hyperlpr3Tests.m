2968a088ccab2be6ccdb70472fc1c616cd84f5a9
tunm <tunmxy@163.com>
2023-03-13 21:58:51 +0800

    Add iOS Libraries Prj

---
+++ Prj-iOS/hyperlpr3/hyperlpr3Tests/hyperlpr3Tests.m
@@ -0,0 +1,36 @@
+//
+//  hyperlpr3Tests.m
+//  hyperlpr3Tests
+//
+//  Created by tunm on 2023/3/13.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface hyperlpr3Tests : XCTestCase
+
+@end
+
+@implementation hyperlpr3Tests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
