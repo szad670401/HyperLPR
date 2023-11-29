01b8307559c15511538a14a86726570b8e6116fd
JingyuYan <tunmxy@163.com>
2023-11-29 19:43:11 +0800

    add pred

--- Prj-Linux/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_fa0ac.dir/CMakeCXXCompilerABI.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/cmTC_fa0ac.dir/CMakeCXXCompilerABI.cpp.o: \
-  /usr/local/Cellar/cmake/3.23.2/share/cmake/Modules/CMakeCXXCompilerABI.cpp \
-  /usr/local/Cellar/cmake/3.23.2/share/cmake/Modules/CMakeCompilerABI.h
--- Prj-Linux/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_fa0ac.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for cmTC_fa0ac.
-# This may be replaced when dependencies are built.
