889061287e1363636d9f12f07e2ae2d0bbb009da
tunm <tunmxy@163.com>
2025-02-28 14:54:02 +0800

    dev

--- Prj-Python/hyperlpr3/__init__.py
+++ Prj-Python/hyperlpr3/__init__.py
@@ -2,6 +2,6 @@ from .algo.pallet import *
 from .hyperlpr3 import LicensePlateCatcher
 from . import helper
 
-__version__ = "0.2.0"
+__version__ = "1.3.0"
 
 print(f"[HyperLPRv3-v{__version__}]For more information about HyperLPR please visit: https://github.com/szad670401/HyperLPR\n")
